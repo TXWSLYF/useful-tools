55a4e65cc9d05558c55a724ac9fc2aa2ed3c2400
alexluyufeng <alexluyufeng@didiglobal.com>
2019-05-13 22:44:39 +0800

    build: build

---
+++ build/precache-manifest.a471fcb40a03f1a95e685da26bf7b99b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31493c5118988b9808cbe329e3d29965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ad7dc4a9426b56b8f41",
+    "url": "/static/css/main.e9b17471.chunk.css"
+  },
+  {
+    "revision": "e31f464b41999ff466a3",
+    "url": "/static/js/2.9cc174a6.chunk.js"
+  },
+  {
+    "revision": "9ad7dc4a9426b56b8f41",
+    "url": "/static/js/main.2aab742b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
